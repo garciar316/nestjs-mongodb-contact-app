d8dc3372270dd02fc802f58781bea364e0479425
garciar316 <sergio980316@hotmail.com>
2022-09-07 20:25:14 -0500

    ADD: servicio y controlador para Contacto

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,114 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Res,
+  Query,
+} from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+  constructor(private contactService: ContactService) {}
+
+  @Post()
+  async createContact(@Res() res, @Body() createContactDto: CreateContactDto) {
+    try {
+      const newContact = await this.contactService.createContact(
+        createContactDto,
+      );
+      return res.status(HttpStatus.CREATED).json({
+        message: 'Se ha creado el contacto',
+        newContact,
+      });
+    } catch (err) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'Error: no se creo el contacto',
+        error: err.message,
+      });
+    }
+  }
+
+  @Put(':id')
+  async updateContact(
+    @Res() res,
+    @Param('id') ContactId: string,
+    @Body() updateContactDto: UpdateContactDto,
+  ) {
+    try {
+      const existingContact = await this.contactService.updateContact(
+        ContactId,
+        updateContactDto,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Contacto actualizado exitosamente',
+        existingContact,
+      });
+    } catch (err) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Ha ocurrido un error, no se ha actualizado el contacto',
+        error: err.message,
+      });
+    }
+  }
+
+  @Get()
+  async getContacts(@Res() res) {
+    try {
+      const contactData = await this.contactService.getContacts();
+      return res.status(HttpStatus.OK).json({
+        message: 'Contactos encontrados',
+        contactData,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Get(':id')
+  async getContactById(@Res() res, @Param('id') contactId: string) {
+    try {
+      const contact = await this.contactService.getContactById(contactId);
+      return res.status(HttpStatus.OK).json({
+        message: 'Contacto encontrado',
+        contact,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Get()
+  async getContactsByUserId(@Res() res, @Query('userId') userId) {
+    try {
+      const contacts = await this.contactService.getContactsByUserId(userId);
+      return res.status(HttpStatus.OK).json({
+        message: 'Contactos encontrados',
+        contacts,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Delete(':id')
+  async deleteContact(@Res() res, @Param('id') contactId: string) {
+    try {
+      const deleteContact = await this.contactService.deleteContact(contactId);
+      return res.status(HttpStatus.OK).jso({
+        message: 'Contacto eliminado corectamente',
+        deleteContact,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+}
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { UpdateContactDto } from './dto/update-contact.dto';
+import { IContact } from './interface/contact.interface';
 
 @Injectable()
-export class ContactService {}
+export class ContactService {
+  constructor(@InjectModel('Contact') private contactModel: Model<IContact>) {}
+
+  async createContact(createContactDto: CreateContactDto): Promise<IContact> {
+    const newContact = new this.contactModel(createContactDto);
+    return newContact.save();
+  }
+
+  async updateContact(
+    contactId: string,
+    updateContactDto: UpdateContactDto,
+  ): Promise<IContact> {
+    const existingContact = this.contactModel.findByIdAndUpdate(
+      contactId,
+      updateContactDto,
+      { new: true },
+    );
+
+    if (!existingContact) {
+      throw new NotFoundException(
+        `No se encontro el contacto con id ${contactId}`,
+      );
+    }
+
+    return existingContact;
+  }
+
+  getContacts(): Promise<IContact[]> {
+    const contacts = this.contactModel.find().exec();
+    if (!contacts) {
+      throw new NotFoundException(`No se encontraron contactos`);
+    }
+    return contacts;
+  }
+
+  async getContactById(contactId: string): Promise<IContact> {
+    const existingContact = this.contactModel.findById(contactId).exec();
+    if (!existingContact) {
+      throw new NotFoundException(
+        `No se encontro el contacto con id ${contactId}`,
+      );
+    }
+    return existingContact;
+  }
+
+  async getContactsByUserId(userId: string): Promise<IContact[]> {
+    const contacts = (await this.contactModel.find()).filter(
+      (c) => c.user_id === parseInt(userId),
+    );
+    if (!contacts) {
+      throw new NotFoundException(
+        `No se encontraron contactos para usuario con id ${userId}`,
+      );
+    }
+    return contacts;
+  }
+
+  async deleteContact(contactId: string): Promise<IContact> {
+    const deletedContact = this.contactModel.findByIdAndDelete(contactId);
+    if (!deletedContact) {
+      throw new NotFoundException(
+        `No se encontro el contacto con id ${contactId}`,
+      );
+    }
+    return deletedContact;
+  }
+}
