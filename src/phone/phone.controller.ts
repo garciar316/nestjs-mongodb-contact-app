2004837662c045d51d3d517483044ed2a391a38e
garciar316 <sergio980316@hotmail.com>
2022-09-07 21:34:27 -0500

    ADD: servicio y controlador para contacto

--- src/phone/phone.controller.ts
+++ src/phone/phone.controller.ts
@@ -1,4 +1,112 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Res,
+  Query,
+} from '@nestjs/common';
+import { CreatePhoneDto } from './dto/create-phone.dto';
+import { UpdatePhoneDto } from './dto/update-phone.dto';
+import { PhoneService } from './phone.service';
 
 @Controller('phone')
-export class PhoneController {}
+export class PhoneController {
+  constructor(private phoneService: PhoneService) {}
+
+  @Post()
+  async createPhone(@Res() res, @Body() createPhoneDto: CreatePhoneDto) {
+    try {
+      const newPhone = await this.phoneService.createPhone(createPhoneDto);
+      return res.status(HttpStatus.CREATED).json({
+        message: 'Se ha creado el teléfono',
+        newPhone,
+      });
+    } catch (err) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'Error: no se creo el teléfono',
+        error: err.message,
+      });
+    }
+  }
+
+  @Put(':id')
+  async updatePhone(
+    @Res() res,
+    @Param('id') PhoneId: string,
+    @Body() updatePhonetDto: UpdatePhoneDto,
+  ) {
+    try {
+      const existingPhone = await this.phoneService.updatePhone(
+        PhoneId,
+        updatePhonetDto,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Teléfono actualizado exitosamente',
+        existingPhone,
+      });
+    } catch (err) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Ha ocurrido un error, no se ha actualizado el teléfono',
+        error: err.message,
+      });
+    }
+  }
+
+  @Get()
+  async getPhones(@Res() res) {
+    try {
+      const phonetData = await this.phoneService.getPhones();
+      return res.status(HttpStatus.OK).json({
+        message: 'Teléfonos encontrados',
+        phonetData,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Get(':id')
+  async getPhoneById(@Res() res, @Param('id') phoneId: string) {
+    try {
+      const teléfono = await this.phoneService.getPhonetById(phoneId);
+      return res.status(HttpStatus.OK).json({
+        message: 'Teléfono encontrado',
+        teléfono,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Get()
+  async getPhonesByContactId(@Res() res, @Query('contactId') contactId) {
+    try {
+      const phones = await this.phoneService.getPhonesByContactId(contactId);
+      return res.status(HttpStatus.OK).json({
+        message: 'Telefonos encontrados',
+        phones,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+
+  @Delete(':id')
+  async deletePhone(@Res() res, @Param('id') phoneId: string) {
+    try {
+      const deletedPhone = await this.phoneService.deletePhone(phoneId);
+      return res.status(HttpStatus.OK).jso({
+        message: 'Teléfono eliminado corectamente',
+        deletedPhone,
+      });
+    } catch (err) {
+      return res.status(err.status).json(err.response);
+    }
+  }
+}
--- src/phone/phone.service.ts
+++ src/phone/phone.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreatePhoneDto } from './dto/create-phone.dto';
+import { UpdatePhoneDto } from './dto/update-phone.dto';
+import { IPhone } from './interface/phone.interface';
 
 @Injectable()
-export class PhoneService {}
+export class PhoneService {
+  constructor(@InjectModel('Phone') private phoneModel: Model<IPhone>) {}
+
+  async createPhone(createPhoneDto: CreatePhoneDto): Promise<IPhone> {
+    const newPhone = new this.phoneModel(createPhoneDto);
+    return newPhone.save();
+  }
+
+  async updatePhone(
+    phoneId: string,
+    updatePhoneDto: UpdatePhoneDto,
+  ): Promise<IPhone> {
+    const existingPhone = this.phoneModel.findByIdAndUpdate(
+      phoneId,
+      updatePhoneDto,
+      { new: true },
+    );
+
+    if (!existingPhone) {
+      throw new NotFoundException(
+        `No se encontro un telefono con id ${phoneId}`,
+      );
+    }
+
+    return existingPhone;
+  }
+
+  getPhones(): Promise<IPhone[]> {
+    const phones = this.phoneModel.find().exec();
+    if (!phones) {
+      throw new NotFoundException(`No se encontraron telefonos`);
+    }
+    return phones;
+  }
+
+  async getPhonetById(phoneId: string): Promise<IPhone> {
+    const existingPhone = this.phoneModel.findById(phoneId).exec();
+    if (!existingPhone) {
+      throw new NotFoundException(
+        `No se encontro el telefono con id ${phoneId}`,
+      );
+    }
+    return existingPhone;
+  }
+
+  async getPhonesByContactId(phoneId: string): Promise<IPhone[]> {
+    const phones = (await this.phoneModel.find()).filter(
+      (p) => p.contact_id === parseInt(phoneId),
+    );
+    if (!phones) {
+      throw new NotFoundException(
+        `No se encontraron telefonos para usuario con id ${phoneId}`,
+      );
+    }
+    return phones;
+  }
+
+  async deletePhone(phoneId: string): Promise<IPhone> {
+    const deletedPhone = this.phoneModel.findByIdAndDelete(phoneId);
+    if (!deletedPhone) {
+      throw new NotFoundException(
+        `No se encontro el teléfono con id ${phoneId}`,
+      );
+    }
+    return deletedPhone;
+  }
+}
